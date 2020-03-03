f79918325ad23587c2b064527ed6ede60f29b1c0
JamunaK <jamunakan@gmail.com>
2020-03-02 19:26:49 -0500

    Updated

--- src/test/java/com/base/BaseClass.java
+++ src/test/java/com/base/BaseClass.java
@@ -40,9 +40,14 @@ public class BaseClass
 		logger.debug("Debug logging has started ");
         System.setProperty("webdriver.chrome.driver", "C:\\chromedriver.exe");
 		ChromeOptions options = new ChromeOptions();
-		options.addArguments("--disable-features=VizDisplayCompositor");
-		WebDriver driver = new ChromeDriver(options);
-
+		options.addArguments("start-maximized"); // https://stackoverflow.com/a/26283818/1689770
+		options.addArguments("enable-automation"); // https://stackoverflow.com/a/43840128/1689770
+		options.addArguments("--no-sandbox"); //https://stackoverflow.com/a/50725918/1689770
+		options.addArguments("--disable-infobars"); //https://stackoverflow.com/a/43840128/1689770
+		options.addArguments("--disable-dev-shm-usage"); //https://stackoverflow.com/a/50725918/1689770
+		options.addArguments("--disable-browser-side-navigation"); //https://stackoverflow.com/a/49123152/1689770
+		options.addArguments("--disable-gpu"); //https://stackoverflow.com/questions/51959986/how-to-solve-selenium-chromedriver-timed-out-receiving-message-from-renderer-exc
+		driver = new ChromeDriver(options);
 		driver.get(baseURL);
         driver.manage().window().maximize();
 	}
